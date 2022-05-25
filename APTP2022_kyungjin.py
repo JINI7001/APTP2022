625f33f00ad9469c2ee9ff2ab839966458887eb2
Kyungjini <hello2060102@yonsei.ac.kr>
2022-05-25 23:52:16 +0900

    220525 minterm과 binary 같이 column1에 표현

--- APTP2022_kyungjin.py
+++ APTP2022_kyungjin.py
@@ -189,8 +189,6 @@ def classification_group(a, lst) :          ### a: 2진수의 자리수(ex: 0000
         append_list=[i,minterm_to_binary(a,i)]
         finallist[numsort(i)].append(append_list)
     return finallist
-
-list1=classification_group(4,[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])
 ###################################classification_group#########################################
 
 ###################################visualization################################################
@@ -288,6 +286,7 @@ def visualization_implicants(arr):
         for i in arr:
             cnt_i += 1
 
+            ### column 표시
             text2 = game_font.render(("Column " + str(cnt_i)), True, (28, 0, 0))
             size_text2 = text2.get_rect()
             size_text2.x = (screen_width / (len(arr) + 1)) * cnt_i + 40
@@ -315,14 +314,16 @@ def visualization_implicants(arr):
                 for k in j:
                     cnt_row += 1
 
-                    text3 = game_font.render(k, True, (28, 0, 0))
+                    ### implicant 표시
+                    k_to_string=str(k[0])+"  "+k[1]
+                    text3 = game_font.render(k_to_string, True, (28, 0, 0))
                     size_text3 = text3.get_rect()
                     size_text3.x = (screen_width / (len(arr) + 1)) * cnt_i + 40
                     size_text3.centery = size_text2.centery + (size_font) * 1.5 * (cnt_row)
                     screen.blit(text3, size_text3)
-
-                pygame.draw.line(screen, (28, 0, 0), (size_text3.x, size_text3.centery + ((size_font) * 1.5) / 2),
-                                 (size_text3.x + 50, size_text3.centery + ((size_font) * 1.5) / 2), 1)
+                ### group 구분선 표시
+                pygame.draw.line(screen, (28, 0, 0), (size_text3.x-7, size_text3.centery + ((size_font) * 1.5) / 2),
+                                 (size_text3.x + size_text3.width+5, size_text3.centery + ((size_font) * 1.5) / 2), 1)
 
         ### 화면에 표시
         screen.blit(text1_rectangle, size_text1_rectangle)
@@ -356,283 +357,12 @@ def visualization_implicants(arr):
     ## pygame 종료
     pygame.quit()
 
-arr1=[[['0000', '0001', '0000', '0100', '0000', '0000'], ['0101', '1111'], ['1010', '1111'],
-                ['0000', '0001', '0100', '1010', '1111']],
-               [['0000', '0001', '0100', '1010', '1111'], ['0000', '0101', '1111'], ['1010', '1111', '1010', '1111']],
-               [['0000', '0001', '0100', '1010'], ['0101', '1111'], ['1010']]]
+list1=classification_group(4,[0,1,2,3,4,5,6,12,13,14,15])
+arr1=[]
+arr1.append(list1)
+arr1.append(list1)
+arr1.append(list1)
 
 visualization_implicants((arr1))
 
 ###################################visualization################################################
-
-######################################binary to decimal###############
-'''
-
-
-'''
-import pygame
-
-pygame.init()
-
-# 화면 크기 설정
-screen_width=800
-screen_height=800
-screen=pygame.display.set_mode((screen_width,screen_height))
-
-# 화면 타이틀 설정
-pygame.display.set_caption(("Quine-Mclusky Method"))
-
-# 폰트 정의
-game_font = pygame.font.Font(None, 40)
-
-# 총 시간
-total_time = 100
-
-# 시작 시간
-start_ticks = pygame.time.get_ticks()
-
-
-##< 충돌게임 >
-
-# FPS
-clock = pygame.time.Clock()
-##################################################################
-background=pygame.image.load("C:/Users/이경진/Desktop/과제2/map.png")
-character=pygame.image.load("C:/Users/이경진/Desktop/과제2/mario_stand.jpg")
-character=character
-character_size=character.get_rect().size
-character_width=character_size[0]
-character_height=character_size[1]
-character_x_pos=(screen_width-character_height)/2
-character_y_pos=screen_height-character_height+100
-
-to_x=0
-to_y=0
-
-character_speed=0.6
-
-# 적 enemy 캐릭터
-enemy=pygame.image.load("C:/Users/이경진/Desktop/과제2/mario_stand.jpg")
-enemy_size = enemy.get_rect().size # 이미지의 크기를 구해옴
-enemy_width = enemy_size[0] # 캐릭터의 가로 크기
-enemy_height = enemy_size[1] # 캐릭터의 세로 크기
-enemy_x_pos = (screen_width - enemy_width) / 2 # 화면 가로의 절반 크기에 해당하는 곳에 위치
-enemy_y_pos = (screen_height - enemy_height) / 2 # 화면 세로의 크기 가장 아래에 해당하는 곳에 위치
-
-
-
-# 이벤트 루프
-running = True
-while running:
-    dt = clock.tick(60)  # 게임화면의 초당 프레임 수를 설정
-
-    # 2. 이벤트 처리 (키보드, 마우스 등)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-        if event.type == pygame.KEYDOWN:  # 키가 눌러졌는지 확인
-            if event.key == pygame.K_LEFT:  # 캐릭터를 왼쪽으로
-                to_x -= character_speed
-            elif event.key == pygame.K_RIGHT:  # 캐릭터를 오른쪽으로
-                to_x += character_speed
-            elif event.key == pygame.K_UP:  # 캐릭터를 위로
-                to_y -= character_speed
-            elif event.key == pygame.K_DOWN:  # 캐릭터를 아래로
-                to_y += character_speed
-        if event.type == pygame.KEYUP:  # 방향키를 떼면 멈춤
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                to_x = 0
-            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                to_y = 0
-
-    # 3. 게임 캐릭터 위치 정의
-    character_x_pos += to_x * dt
-    character_y_pos += to_y * dt
-
-    # 경계값 처리 (캐릭터가 화면 밖을 벗어나지 않도록)
-    if character_x_pos < 0:
-        character_x_pos = 0
-    elif character_x_pos > screen_width - character_width:
-        character_x_pos = screen_width - character_width
-
-    if character_y_pos < 0:
-        character_y_pos = 0
-    elif character_y_pos > screen_height - character_height:
-        character_y_pos = screen_height - character_height
-
-    # 4. 충돌 처리
-    # 충돌 처리를 위한 rect 정보 업데이트
-    character_rect = character.get_rect()
-    character_rect.left = character_x_pos
-    character_rect.top = character_y_pos
-
-    enemy_rect = enemy.get_rect()
-    enemy_rect.left = enemy_x_pos
-    enemy_rect.top = enemy_y_pos
-
-    # 충돌 체크
-    if character_rect.colliderect(enemy_rect):
-        print("충돌했어요")
-        running = False
-
-    # 5. 화면 그리기
-    screen.blit(background, (0, 0))  # 배경 그리기
-    screen.blit(character, (character_x_pos, character_y_pos))  # 캐릭터 그리기
-    screen.blit(enemy, (enemy_x_pos, enemy_y_pos))  # 적 그리기
-
-    # 타이머 집어 넣기
-    # 경과 시간 계산
-    elapsed_time = (pygame.time.get_ticks() - start_ticks) / 1000
-    # 경과 시간(ms)을 1000으로 나누어서 초 단위로 표시
-
-    timer = game_font.render(str(int(total_time - elapsed_time)), True, (255, 255, 255))
-
-    # 출력할 글자, True, 글자 색상
-    screen.blit(timer, (10, 10))
-
-    if total_time - elapsed_time <= 0:
-        print("타임 아웃")
-        running = False
-
-    # 만약 시간이 0 이하이면 게임 종료
-
-    pygame.display.update()  # 게임화면을 다시 그리기
-
-# 잠시 대기
-pygame.time.delay(2000)
-
-# pygame 종료
-pygame.quit()
-
-menu="start"
-homescreen_image=pygame.image.load("./matrice.png").convert_alpha()
-font=pygame.font.SysFont("draglinebtndm",60)
-
-pressed_keys=pygame.key.get_pressed()
-
-
-b=1
-if (b==1):
-    # 1. 사용자 게임 초기화 (배경 화면, 게임 이미지, 좌표, 속도, 폰트 등)
-
-    # 배경 이미지 불러오기
-    background = pygame.image.load("C:/Users/이경진/Desktop/background.png")
-
-    # 캐릭터(스프라이트) 불러오기
-    character = pygame.image.load("C:/Users/이경진/Desktop/123.png")
-    character_size = character.get_rect().size  # 이미지의 크기를 구해옴
-    character_width = character_size[0]  # 캐릭터의 가로 크기
-    character_height = character_size[1]  # 캐릭터의 세로 크기
-    character_x_pos = (screen_width - character_width) / 2  # 화면 가로의 절반 크기에 해당하는 곳에 위치
-    character_y_pos = screen_height - character_height  # 화면 세로의 크기 가장 아래에 해당하는 곳에 위치
-
-    # 이동할 좌표
-    to_x = 0
-    to_y = 0
-
-    # 이동 속도
-    character_speed = 0.6
-
-    # 적 enemy 캐릭터
-    enemy = pygame.image.load("C:/Users/이경진/Desktop/game_start.png")
-    enemy_size = enemy.get_rect().size  # 이미지의 크기를 구해옴
-    enemy_width = enemy_size[0]  # 캐릭터의 가로 크기
-    enemy_height = enemy_size[1]  # 캐릭터의 세로 크기
-    enemy_x_pos = (screen_width - enemy_width) / 2  # 화면 가로의 절반 크기에 해당하는 곳에 위치
-    enemy_y_pos = (screen_height - enemy_height) / 2  # 화면 세로의 크기 가장 아래에 해당하는 곳에 위치
-
-    # 폰트 정의
-    game_font = pygame.font.Font(None, 40)
-
-    # 총 시간
-    total_time = 10
-
-    # 시작 시간
-    start_ticks = pygame.time.get_ticks()
-
-    # 이벤트 루프
-    running = True
-    while running:
-        dt = clock.tick(60)  # 게임화면의 초당 프레임 수를 설정
-
-        # 2. 이벤트 처리 (키보드, 마우스 등)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-            if event.type == pygame.KEYDOWN:  # 키가 눌러졌는지 확인
-                if event.key == pygame.K_LEFT:  # 캐릭터를 왼쪽으로
-                    to_x -= character_speed
-                elif event.key == pygame.K_RIGHT:  # 캐릭터를 오른쪽으로
-                    to_x += character_speed
-                elif event.key == pygame.K_UP:  # 캐릭터를 위로
-                    to_y -= character_speed
-                elif event.key == pygame.K_DOWN:  # 캐릭터를 아래로
-                    to_y += character_speed
-
-            if event.type == pygame.KEYUP:  # 방향키를 떼면 멈춤
-                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                    to_x = 0
-                elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                    to_y = 0
-
-        # 3. 게임 캐릭터 위치 정의
-        character_x_pos += to_x * dt
-        character_y_pos += to_y * dt
-
-        # 경계값 처리 (캐릭터가 화면 밖을 벗어나지 않도록)
-        if character_x_pos < 0:
-            character_x_pos = 0
-        elif character_x_pos > screen_width - character_width:
-            character_x_pos = screen_width - character_width
-
-        if character_y_pos < 0:
-            character_y_pos = 0
-        elif character_y_pos > screen_height - character_height:
-            character_y_pos = screen_height - character_height
-
-        # 4. 충돌 처리
-        # 충돌 처리를 위한 rect 정보 업데이트
-        character_rect = character.get_rect()
-        character_rect.left = character_x_pos
-        character_rect.top = character_y_pos
-
-        enemy_rect = enemy.get_rect()
-        enemy_rect.left = enemy_x_pos
-        enemy_rect.top = enemy_y_pos
-
-        # 충돌 체크
-        if character_rect.colliderect(enemy_rect):
-            print("충돌했어요")
-            running = False
-
-        # 5. 화면 그리기
-        screen.blit(background, (0, 0))  # 배경 그리기
-        screen.blit(character, (character_x_pos, character_y_pos))  # 캐릭터 그리기
-        screen.blit(enemy, (enemy_x_pos, enemy_y_pos))  # 적 그리기
-
-        # 타이머 집어 넣기
-        # 경과 시간 계산
-        elapsed_time = (pygame.time.get_ticks() - start_ticks) / 1000
-        # 경과 시간(ms)을 1000으로 나누어서 초 단위로 표시
-
-        timer = game_font.render(str(int(total_time - elapsed_time)), True, (255, 255, 255))
-
-        # 출력할 글자, True, 글자 색상
-        screen.blit(timer, (10, 10))
-
-        if total_time - elapsed_time <= 0:
-            print("타임 아웃")
-            running = False
-
-        # 만약 시간이 0 이하이면 게임 종료
-
-        pygame.display.update()  # 게임화면을 다시 그리기
-
-    # 잠시 대기
-    pygame.time.delay(100)
-
-    # pygame 종료
-    pygame.quit()
-'''
